f7b99ada1982d8d3d80a61f34ac75286eda00e3f
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-19 11:51:23 +0800

    Update config order

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,39 +2,30 @@
 
 # Usage: Copy the file to "$PROFILE" or "C:\Users\xxx\Documents\PowerShell\"
 
-#Aliases
-Set-Alias tt tree
-Set-Alias ll ls
-Set-Alias g git
-Set-Alias vim nvim
+#Functions
+function whereis ($command) {
+	Get-Command -Name $command -ErrorAction SilentlyContinue |
+	Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+function Invoke-Exa {
+	Invoke-Expression "E:/GitHub/exa/target/debug/exa.exe $args"
+}
 
 #Prompt
 oh-my-posh init pwsh --config 'C:\Users\dachu\AppData\Local\Programs\oh-my-posh\themes\jandedobbeleer.omp.json' | Invoke-Expression
 
 #PSReadLineOption
 Import-Module PSReadLine
-# Set-PSReadlineKeyHandler -Key Tab -Function Complete
+#Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineOption -PredictionViewStyle ListView
 
 #Terminal Icons
 Import-Module Terminal-Icons
 
-#Functions
-function whereis ($command) {
-	Get-Command -Name $command -ErrorAction SilentlyContinue |
-	Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-}
-
-# Exa
-# To deploy: head to <https://github.com/ogham/exa#development-------->
-# 1. Clone the repo locally
-# 2. Run `cargo build`
-# 3. Run `cargo test`
-# 4. Copy executable filepath
-# 5. Paste filepath into the function below
-function Invoke-Exa {
-	Invoke-Expression "E:/GitHub/exa/target/debug/exa.exe $args"
-}
-
+#Aliases
+Set-Alias tt tree
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias vim nvim
 Set-Alias exa Invoke-Exa
 Set-Alias ls Invoke-Exa
